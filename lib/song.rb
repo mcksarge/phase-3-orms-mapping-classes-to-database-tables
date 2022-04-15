fa2887bd1bf7f7c157fd826a8f221164f60dc65b
Fred <higgins.fred.s@gmail.com>
2022-04-15 10:12:34 -0700

    commit tree 8e34729442d4c687dcccc4b806d9f6a11a2aa7ef

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,42 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
+  end
+
+  def self.create_table
+    sql =
+      "CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )"
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = 
+    "
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    "
+
+    DB[:conn].execute(sql, self.name, self.album)
+
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    self
+
+  end
+
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
   end
 
 end
